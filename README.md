b06852bb033edb77a455094bab2167f1352804f2
joe <joseph.mcevoy1984@gmail.com>
2016-12-27 17:22:41 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Django-Unleashed
+
+Project from the 2015 book "Django Unleashed" by Andrew Pinkham
+https://django-unleashed.com/
+
+This is the Django project I am currently working through from the above book.
+
+# Current Chapter: Chapter 5 - Creating Webpages with Controllers in Django (Chp: 5.6)
---
+++ suorganizer/organizer/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse, Http404
+from django.template import Context, loader
+
+from .models import Tag
+
+
+def homepage(request):
+    tag_list = Tag.objects.all()
+    template = loader.get_template(
+        'organizer/tag_list.html')
+    context = Context({'tag_list': tag_list})
+    output = template.render(context)
+    return HttpResponse(output)
+
+def tag_detail(request, slug):
+    try:
+        tag = Tag.objects.get(slug__iexact=slug)
+    except Tag.DoesNotExist:
+        raise Http404
+    template = loader.get_template(
+        'organizer/tag_detail.html')
+    context = Context({'tag': tag})
+    return HttpResponse(template.render(context))
