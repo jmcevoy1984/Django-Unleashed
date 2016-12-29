d066205d34beb6b6c96cdb76ea8aa9a833189e2f
joe <joseph.mcevoy1984@gmail.com>
2016-12-29 17:32:00 +0000

    second commit

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 Project from the 2015 book "Django Unleashed" by Andrew Pinkham
 https://django-unleashed.com/
 
+The book walks through an example of a startup organizer project containing multiple apps.
+
 This is the Django project I am currently working through from the above book.
 
-# Current Chapter: Chapter 5 - Creating Webpages with Controllers in Django (Chp: 5.6)
+# Current Chapter: Chapter 5 - Creating Webpages with Controllers in Django (Chp: 5.8.3)
--- suorganizer/organizer/views.py
+++ suorganizer/organizer/views.py
@@ -1,23 +1,23 @@
-from django.http import HttpResponse, Http404
-from django.template import Context, loader
+from django.shortcuts import get_object_or_404, render
 
-from .models import Tag
+from .models import Startup, Tag
 
+def startup_list(request):
+    return render(
+        request,
+        'organizer/startup_list.html',
+        {'startup_list': Startup.objects.all()})
 
-def homepage(request):
-    tag_list = Tag.objects.all()
-    template = loader.get_template(
-        'organizer/tag_list.html')
-    context = Context({'tag_list': tag_list})
-    output = template.render(context)
-    return HttpResponse(output)
+def tag_list(request):
+    return render(
+        request,
+        'organizer/tag_list.html',
+        {'tag_list': Tag.objects.all()})
 
 def tag_detail(request, slug):
-    try:
-        tag = Tag.objects.get(slug__iexact=slug)
-    except Tag.DoesNotExist:
-        raise Http404
-    template = loader.get_template(
-        'organizer/tag_detail.html')
-    context = Context({'tag': tag})
-    return HttpResponse(template.render(context))
+    tag = get_object_or_404(
+        Tag, slug__iexact=slug)
+    return render(
+        request,
+        'organizer/tag_detail.html',
+        {'tag': tag})
