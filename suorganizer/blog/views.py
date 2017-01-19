da7392b0243e3516d42204dabb4abe4365ce74a7
joe <joseph.mcevoy1984@gmail.com>
2017-01-19 21:06:56 +0000

    chapter-6.1

--- suorganizer/blog/views.py
+++ suorganizer/blog/views.py
@@ -5,15 +5,6 @@ from django.views.generic import View
 from .models import Post
 
 
-class PostList(View):
-
-    def get(self, request):
-        return render(
-            request,
-            'blog/post_list.html',
-            {'post_list': Post.objects.all()})
-
-
 def post_detail(request, year, month, slug):
     post = get_object_or_404(
         Post,
@@ -24,3 +15,11 @@ def post_detail(request, year, month, slug):
         request,
         'blog/post_detail.html',
         {'post': post})
+
+class PostList(View):
+
+    def get(self, request):
+        return render(
+            request,
+            'blog/post_list.html',
+            {'post_list': Post.objects.all()})
