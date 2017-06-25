de425e7d7b33f7427660f48888b2aeb8ba5175e9
joe <joseph.mcevoy1984@gmail.com>
2017-06-25 12:51:23 +0100

    chapter-9.3

--- suorganizer/blog/views.py
+++ suorganizer/blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import (
-    get_object_or_404, render)
+    get_object_or_404, redirect, render)
 from django.views.generic import View
 
 from .models import Post
+from .forms import PostForm
 
 
 def post_detail(request, year, month, slug):
@@ -23,3 +24,24 @@ class PostList(View):
             request,
             'blog/post_list.html',
             {'post_list': Post.objects.all()})
+
+class PostCreate(View):
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def get(self, request):
+        return render(
+            request,
+            self.template_name,
+            {'form': self.form_class()})
+
+    def post(self, request):
+        bound_form = self.form_class(request.POST)
+        if bound_form.is_valid():
+            new_post = bound_form.save()
+            return redirect(new_post)
+        else:
+            return render(
+                request,
+                self.template_name,
+                {'form': bound_form})
--- suorganizer/organizer/views.py
+++ suorganizer/organizer/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import (
+    get_object_or_404, render, redirect)
+from django.views.generic import View
 
 from .models import Startup, Tag
+from .forms import NewsLinkForm, StartupForm, TagForm
+from .utils import ObjectCreateMixin
+
+
+class NewsLinkCreate(ObjectCreateMixin, View):
+    form_class = NewsLinkForm
+    template_name = 'organizer/newslink_form.html'
+
+
+class StartupCreate(ObjectCreateMixin, View):
+    form_class = StartupForm
+    template_name = 'organizer/startup_form.html'
+
 
 def startup_list(request):
     return render(
         request,
         'organizer/startup_list.html',
         {'startup_list': Startup.objects.all()})
+
 
 def startup_detail(request, slug):
     startup = get_object_or_404(
@@ -16,11 +32,13 @@ def startup_detail(request, slug):
         'organizer/startup_detail.html',
         {'startup': startup})
 
+
 def tag_list(request):
     return render(
         request,
         'organizer/tag_list.html',
         {'tag_list': Tag.objects.all()})
+
 
 def tag_detail(request, slug):
     tag = get_object_or_404(
@@ -29,3 +47,8 @@ def tag_detail(request, slug):
         request,
         'organizer/tag_detail.html',
         {'tag': tag})
+
+
+class TagCreate(ObjectCreateMixin, View):
+    form_class = TagForm
+    template_name = 'organizer/tag_form.html'
